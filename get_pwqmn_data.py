eeaafd927a90e60df783d15ee5dd508ba458c96d
dsartori <doug@sartori.ca>
2023-07-17 15:10:17 -0400

    presentation fixes
    graph fixes

--- get_pwqmn_data.py
+++ get_pwqmn_data.py
@@ -18,11 +18,22 @@ joinedTable = petl.join(observationTable,stationTable, lkey='Collection Site',rk
 leadTable = petl.select(joinedTable, lambda rec: rec['Analyte'] == 'Lead')
 
 # Generate a chart for each station
-for station in petl.values(leadTable, 'Collection Site'):
+for station, name in petl.values(leadTable, 'Collection Site', 'NAME'):
+
     # filter the table for the current station
     stationTable = petl.select(leadTable, lambda rec: rec['Collection Site'] == station)
+
+    # concatenate collection date and time into a single field
+    stationTable = petl.addfield(stationTable, 'd', lambda rec: rec['Collection Date'] + ' ' + rec['Collection Time'])
+
+    # sort the table by collection date 
+    stationTable = petl.sort(stationTable, ['d'])
+
+    # remove leading < symbol from result values
+    stationTable = petl.convert(stationTable, 'Result', lambda v: int(v[1:]) if v[0] == '<' else v)
+    
     # generate the chart
-    bcd_chart.generate(petl.values(stationTable, 'Collection Date'), petl.values(stationTable, 'Result'), station, 'Lead Concentration (ug/L)', station + '.png')
+    bcd_chart.generate(petl.values(stationTable, 'd'), petl.values(stationTable, 'Result'), 'Lead Concentration (ug/L)', name,  station + '.png')
 
 # Write the data to a csv file
 petl.tocsv(leadTable, 'pwqmn_data.csv')
